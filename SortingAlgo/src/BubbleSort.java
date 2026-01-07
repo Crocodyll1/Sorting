517cd4c3b157911bcff719dc425d125e7ea2a974
Crocodyl <unknownsushant.0042@gmail.com>
2026-01-07 23:24:46 +0530

    Bubble Sort

--- SortingAlgo/src/BubbleSort.java
+++ SortingAlgo/src/BubbleSort.java
@@ -1,2 +1,19 @@
 public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr={5,4,2,9,77,88,23,54,32,76,11,1};
+        int n = arr.length;
+        for(int i=n-1; i>=1; i--){
+            for(int j=0; j<=i-1; j++){
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j+1];
+                    arr[j+1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        System.out.print("Bubble Sorting : ");
+        for(int i =0; i<n; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
 }
