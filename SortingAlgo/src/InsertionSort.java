fad834c263f56280b1573ca363be60ed766639d6
Crocodyl <unknownsushant.0042@gmail.com>
2026-01-08 21:54:42 +0530

    Insertion Sort

---
+++ SortingAlgo/src/InsertionSort.java
@@ -0,0 +1,23 @@
+public class InsertionSort {
+    public static void main(String[] args) {
+
+
+        int[] arr={4,6,2,9,6,7,4,22,10};
+        int n = arr.length;
+
+        for(int i =0; i<=n-1; i++){
+            int j =i;
+            while(j > 0 && arr[j-1] > arr[j]){
+                int temp = arr[j-1];
+                arr[j-1] =arr[j];
+                arr[j] = temp;
+                j--;
+            }
+        }
+        System.out.println("Selection Sort : ");
+        for(int i =0; i<n; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+}
