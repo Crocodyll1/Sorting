183bf4216f415ea625e4661d5fa8630cefc39a0a
Sushant Yadav <unknownsushant.0042@gmail.com>
2026-01-11 22:06:05 +0530

    largest element in array

---
+++ arrays/src/largestElement.java
@@ -0,0 +1,15 @@
+public class largestElement {
+    public static void main(String[] args) {
+
+        int[] arr= {25,67,1,4,63,89,100};
+        int n = arr.length;
+        int largest = arr[0];
+
+        for(int i =1; i<n; i++){
+            if(arr[i] > largest){
+                largest = arr[i];
+            }
+        }
+        System.out.println(largest);
+    }
+}
